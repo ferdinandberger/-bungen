8f62035f50fc940b1c07f092840333443b7f147f
Ferdinand-Berger <ferdi.berger.fbr@gmail.com>
2025-04-18 15:08:12 +0100

    first commit

---
+++ UE1_Berger_Ferdinand.jl
@@ -0,0 +1,52 @@
+
+#=
+Fuege in dieses Template deine Loesungscodes ein.
+Wichtig 1: Die Datei gemaess der Namensrichtlinien benennen.
+Wichtig 2: Bitte nur die Funktionen ausprogrammieren und keine Funktionsaufrufe vornehmen.
+=#
+
+
+### Beispiel 1:
+
+function greatest(x::Vector{T}, k::Integer = 1) :: Vector{T} where {T <: Real}
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
+
+### Beispiel 2
+
+function nearestindex(x::Vector{<:Real}, y::Real) :: Int
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
+
+### Beispiel 3
+
+function swap!(x::Vector, i::Integer, j::Integer) :: Nothing
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
+function bubblesort!(x::Vector{<:Real}; rev::Bool = false) :: Nothing
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
+
+### Beispiel 4
+
+function canonicaltour(x::Vector{T}) :: Vector{T} where {T <: Integer}
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
+
+### Beispiel 5
+
+function distance(x::Vector{<:Real}, y::Vector{<:Real}; p::Real = 2)
+    # Fuege hier deinen Loesungscode ein
+    return
+end
+
